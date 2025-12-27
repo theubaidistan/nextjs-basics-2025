1dc43700bfe937d9953ea17326c8e0a09d02fde7
theubaidistan <ubaidistan395@gmail.com>
2025-12-27 15:46:22 +0500

    first commit

---
+++ pages/cars/index.js
@@ -0,0 +1,61 @@
+// import Link from "next/link";
+// import styles from "../../styles/Home.module.css";
+
+// export default function CarsList(props) {
+//   return (
+//     <div className={styles.container}>
+//       <main className={styles.main}>
+//         <h1 className={styles.title}>Cars list</h1>
+
+//         <ul>
+//           <li>
+//             <Link href="/cars/tesla" legacyBehavior>
+//               <a>Tesla</a>
+//             </Link>
+//           </li>
+//           <li>
+//             <Link href="/cars/ford" legacyBehavior>
+//               <a>Ford</a>
+//             </Link>
+//           </li>
+//           <li>
+//             <Link href="/cars/lambo" legacyBehavior>
+//               <a>Lambo</a>
+//             </Link>
+//           </li>
+//         </ul>
+//       </main>
+//     </div>
+//   );
+// }
+//*---------------------------------------------------------------------------------------
+import Link from "next/link";
+import styles from "../../styles/Home.module.css";
+
+export default function CarsList() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Cars list</h1>
+
+        <ul className={styles.carList}>
+          <li className={styles.carItem}>
+            <Link href="/cars/Tesla" legacyBehavior>
+              <a className={styles.carLink}>Tesla</a>
+            </Link>
+          </li>
+          <li className={styles.carItem}>
+            <Link href="/cars/Ford" legacyBehavior>
+              <a className={styles.carLink}>Ford</a>
+            </Link>
+          </li>
+          <li className={styles.carItem}>
+            <Link href="/cars/Lamborghinis" legacyBehavior>
+              <a className={styles.carLink}>Lamborghinis</a>
+            </Link>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
---
+++ pages/hi/hello.js
@@ -0,0 +1,5 @@
+import styles from "./Hello.module.css";
+
+export default function Hello() {
+  return <h1 className={styles.container}>hi mom</h1>;
+}
